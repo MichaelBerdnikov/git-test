e5f2cf1353c8c74f04051a5963ee4cd2017cef35
MichaelBerdnikov <mikeberd@mail.ru>
2016-09-22 22:06:52 +0300

    Make communication between two programs

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 
 #include <qfile.h>
+#include <fstream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -23,11 +24,17 @@ MainWindow::~MainWindow()
 
 void MainWindow::updateFile() const
 {
-    QFile file("name.txt");
-    file.open(QIODevice::WriteOnly);
-    QDataStream stream(&file);
-    stream << ui->firstSpinBox->text() << ui->comboBox->currentText() << ui->secondSpinBox->text();
-    file.close();
+    std::ofstream output("name.txt");
+    output << ui->firstSpinBox->text().toLatin1().toStdString()
+           << ui->comboBox->currentText().toLatin1().toStdString()
+           << ui->secondSpinBox->text().toLatin1().toStdString();
+    output.close();
+    std::system("file_calculator.exe");
+    std::ifstream input("result.txt");
+    std::string s;
+    input >> s;
+    ui->outputLabel->setText(QString::fromStdString(s));
+    input.close();
 }
 
 void MainWindow::valueChanged(int i)
